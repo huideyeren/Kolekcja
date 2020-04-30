80dba5532eaafe39ff020988cfc5242764bbb459
huideyeren <takakura.yusuke@gmail.com>
2020-04-30 20:33:15 +0900

    Add Application test project.

---
+++ Kolekcja.Application.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Kolekcja.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
